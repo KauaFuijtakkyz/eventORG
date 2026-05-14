8f8192f99463ccf42859beb571929c59562d9122
Kauã Kyuzo Fujita <271039221+KauaFuijtakkyz@users.noreply.github.com>
2026-05-13 21:19:46 -0300

    test: estruturar pacote de testes unitarios e de integracao

---
+++ src/test/java/kkfujita/dev/eventORG/EventOrgApplicationTests.java
@@ -0,0 +1,13 @@
+package kkfujita.dev.eventORG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventOrgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
