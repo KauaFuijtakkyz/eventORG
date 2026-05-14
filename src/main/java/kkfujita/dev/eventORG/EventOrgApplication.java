90a6d49e5e8551409efbac6bfccaa383ff7d24d4
Kauã Kyuzo Fujita <271039221+KauaFuijtakkyz@users.noreply.github.com>
2026-05-13 21:19:44 -0300

    feat(backend): adicionar classe principal inicializadora do spring boot

---
+++ src/main/java/kkfujita/dev/eventORG/EventOrgApplication.java
@@ -0,0 +1,13 @@
+package kkfujita.dev.eventORG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventOrgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventOrgApplication.class, args);
+	}
+
+}
